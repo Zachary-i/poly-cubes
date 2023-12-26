e4339a31bd7c24e70e8e64b67a591fc1853daa3b
Zachary Inglis <zacharycinglis@gmail.com>
2023-12-25 23:12:32 -0600

    code skeleton, structs, and begining of logic

--- code/polycubes.cpp
+++ code/polycubes.cpp
@@ -4,24 +4,108 @@ struct Cube {
   int x;
   int y;
   int z;
+  Cube(int x = 0, int y = 0, int z = 0): x(x), y(y), z(z){
+  }
 };
 
 struct Shape {
   int numCubes;
   Cube* shapeCubesPtr;
-
-  void tryToAddCube(){
-    
+  int totalDistance;
+  Shape(int nCubes = 0, Cube* cubesPtr = new Cube(),
+	int distance = 0):numCubes(nCubes),
+			       shapeCubesPtr(cubesPtr),
+			       totalDistance(distance){
+    if(totalDistance == 0){
+      totalDistance = calculateTotalDistance();
+    }
   }
-  
+
+private:
+  int calculateTotalDistance(){
+    int result = 0;
+    for (int i=0; i < numCubes; i++){
+      result += ((shapeCubesPtr+i)->x * (shapeCubesPtr+i)->x +
+		 (shapeCubesPtr+i)->y * (shapeCubesPtr+i)->y +
+		 (shapeCubesPtr+i)->z * (shapeCubesPtr+i)->z);
+    }
+    return result;
+  }
 };
 
-int calculateTotalShapes(int n, Cube* shapesPtr, int mapLength){
-  return 5;
+int calculateShapes(int n, Shape* shapesPtr){
+
+  if(n > 1){
+    shapesPtr = new Shape();
+    return 1;
+  } else {
+    
+    int mapLength = n*n;
+    shapesPtr = new Shape[mapLength];
+    Shape* previousShapesPtr = nullptr;
+    int previousLength = (n-1) * (n-1);
+    int totalNumShapes = 0;
+
+    calculateShapes(n-1, previousShapesPtr);
+    
+    for(int i = 0; i < previousLength; i++){
+
+      Shape* currentShapePtr = (previousShapesPtr + i); 
+      Cube* endCubePtr = (currentShapePtr->shapeCubesPtr +
+			  (currentShapePtr->numCubes - 1));
+
+      int endX = endCubePtr->x;
+      int endY = endCubePtr->y;
+      int endZ = endCubePtr->z;
+
+      char testAdditions = 0;
+      // 1 = +x; 2 = -x; 4 = +y; 8 = -y; 16 = +z; 32 = -z;
+      
+      for(int j = 0; j < currentShapePtr->numCubes; j++){
+	Cube* currentCubePtr = currentShapePtr->shapeCubesPtr+j;
+	int x = currentCubePtr->x;
+	int y = currentCubePtr->y;
+	int z = currentCubePtr->z;
+	
+	if (endX+1 == x && endY == y && endZ == z){
+	  testAdditions |= 1;
+	}
+
+	if (endX-1 == x && endY == y && endZ == z){
+	  testAdditions |= 2;
+	}
+
+	if (endX == x && endY+1 == y && endZ == z){
+	  testAdditions |= 4;
+	}
+
+	if (endX == x && endY-1 == y && endZ == z){
+	  testAdditions |= 8;
+	}
+
+	if (endX == x && endY == y && endZ+1 == z){
+	  testAdditions |= 16;
+	}
+
+	if (endX == x && endY == y && endZ-1 == z){
+	  testAdditions |= 32;
+	}
+      }
+
+      if(testAdditions & 1 == 1){
+
+      }
+      
+    }
+    return totalNumShapes;
+  }
+}
+
+void tryToAddCube(Cube* cubePtr, Shape* baseShapePtr, Shape* shapesPtr){
 
 }
 
-void printShapes(Cube* shapesPtr){
+void printShapes(int n, Shape* shapesPtr){
   std::cout << "[0,0,0]";
 }
 
@@ -31,13 +115,15 @@ int main(){
   int n;
   std::cin >> n;
 
-  Cube* shapesPtr = new Cube[n*n];
+  Shape* shapesPtr = nullptr;
   std::cout << "Total number of shapes: " <<
-    calculateTotalShapes(n, shapesPtr, n*n) <<
+    calculateShapes(n, shapesPtr) <<
     "\nWould you like to print the shapes? (y/n) ";
+  
   char printChoice;
   std::cin >> printChoice;
-  if(printChoice=='y'){printShapes(shapesPtr);};
+
+  if(printChoice=='y'){printShapes(n, shapesPtr);};
   
   return 0;
 }
